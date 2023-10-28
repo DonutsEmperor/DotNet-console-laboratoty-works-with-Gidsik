7ba976ff0e1689276f4ee320ec4205a150ce0138
NikitaDonkey <polovykhnikita@gmail.com>
2023-10-28 10:03:05 +0700

    ThreadExamples Added

---
+++ Laba6/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp;
+using System;
+using System.Threading;
+
+
+//SimpleThreadsExample.SyncingThreadsExample_Synced();
+
+
+//SimpleThreadsExample.SyncingThreadsExample_Synced();
+
+
+AdvancedThreadsExample threadsExample = new AdvancedThreadsExample();
+
+threadsExample.InitThreads();
+
+
+bool stopFlag = false;
+while (true)
+{
+	switch (Console.ReadKey(true).Key)
+	{
+		case ConsoleKey.A: threadsExample.StartWork(); break;
+		case ConsoleKey.S: threadsExample.PauseWork(); break;
+		case ConsoleKey.Z: threadsExample.StartAnotherWork(); break;
+		case ConsoleKey.X: threadsExample.PauseAnotherWork(); break;
+		case ConsoleKey.Escape: stopFlag = true; break;
+	}
+	if (stopFlag) break;
+}
+
+threadsExample.Dispose();
+
+Thread.Sleep(1200);
+

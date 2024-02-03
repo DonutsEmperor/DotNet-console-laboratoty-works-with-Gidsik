20ffceea30ec9a65a0a65869c91166ab06883de3
NikitaDonkey <polovykhnikita@gmail.com>
2024-02-03 12:48:25 +0700

    yeah

---
+++ sources/Lab1/WinFormsApp1/MyComponent.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace WinFormsApp1
+{
+    partial class MyComponent
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer = new System.Windows.Forms.Timer(components);
+            // 
+            // timer
+            // 
+            timer.Tick += timer_Tick;
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ sources/Lab1/WinFormsApp1/MyComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public partial class MyComponent : Component
+    {
+        public event EventHandler? Tick;
+        public MyComponent()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public MyComponent(IContainer container)
+        {
+            container.Add(this);
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            timer.Interval = 1000; //mlseconds
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+            Debug.WriteLine($"{DateTime.UtcNow}");
+        }
+    }
+}

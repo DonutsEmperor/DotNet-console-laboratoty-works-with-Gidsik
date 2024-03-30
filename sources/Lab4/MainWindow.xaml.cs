1eeb12df395ead053655f777b70454b6516f0d7f
NikitaDonkey <polovykhnikita@gmail.com>
2024-03-30 12:22:11 +0700

    lab4 start

---
+++ sources/Lab4/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new Person()
+            {
+                Id = 1,
+                Name = string.Empty,
+            };
+
+        }
+    }
+}
---
+++ sources/Lab4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab4
+{
+    class MyApplication : Application
+    {
+        public MyApplication() 
+        {
+            this.Startup += MyOnStartup;
+        }
+
+        protected void MyOnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+    }
+
+    class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var app = new MyApplication();
+            app.Run();
+        }
+    }
+}
